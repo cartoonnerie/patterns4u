0d6f0af59491939e3deb00c0abe0a1357a910fd6
cartoonnerie <melodia@melix.org>
2021-05-25 22:08:26 +0200

    feat T15: install stylelintrc & configure eslintrc

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -35,6 +35,7 @@ export default {
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
+    '@nuxtjs/stylelint-module'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
